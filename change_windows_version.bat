a2ee8c4cd47889f115692a86eb5ed845685a3d10
alienjay <sgoku3636@hotmail.com>
2021-06-11 16:33:13 -0500

    Added description

---
+++ change_windows_version.bat
@@ -0,0 +1,3 @@
+REM Changes the Windows version to 10 Enterprise. Change the 20 version key to whatever version you need. 
+
+Cscript.exe c:\windows\system32\slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
