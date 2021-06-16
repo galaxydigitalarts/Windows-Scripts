bf9d6d635effe6bfc17e22ddc0c11e65f7d3e2a3
alienjay <sgoku3636@hotmail.com>
2021-06-16 15:48:26 -0500

    Added new scripts

---
+++ Ping_test.ps1
@@ -0,0 +1,18 @@
+﻿$Result=Test-Connection 172.19.115.99 -Quiet -Count 1
+
+if ($Result -match "True")
+{
+
+}
+
+else 
+{
+    if ($Result -match "False")
+    {
+        Write-Host "I like orange trees!"
+        Invoke-Item "C:\Users\176245\Desktop\EMERGENCY.png"
+        Invoke-Item "C:\Users\176245\Desktop\offair.mp3"
+    }
+
+}
+
---
+++ Send_email_if_service_not_running.ps1
@@ -0,0 +1,56 @@
+﻿ $myServiceArray=@()
+
+ $myServiceArray="wuauserv","WinRM"
+
+ Foreach($Service in $myServiceArray)
+ {
+ 
+ Try {
+            $Computername=$env:COMPUTERNAME
+            
+            $Result=Get-Service -ComputerName $Computername -Name "$Service" -ErrorAction Stop  | select -ExpandProperty Status
+ 
+            if ($Result -ne "running")
+                {     
+                     
+                   Start-Service -name $Service 
+                     
+                    $Final=Get-Service -ComputerName $Computername -Name "$Service" -ErrorAction Stop  | select -ExpandProperty Status              
+                    #----------------------------
+                    # Configure Mail Variables
+                    #---------------------------   
+
+                    $EmailFrom = "[your send from email]"
+                    $EmailTo = "[your send to email]"
+                    $Subject = "$Computername Error $Service Not Running"
+                    $Body = "Server: $Computername `n Service: $Service is not running. `n ***Attempting to start . . .*** `n Current Status = $Final"
+                    $SMTPServer = "smtp.gmail.com"
+                    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+                    $SMTPClient.EnableSsl = $true
+                    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("[your email prefix]", "[your email password]");
+                    $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+                }
+      }
+
+Catch [Exception]
+        {      
+            #----------------------------
+            # Configure Mail Variables
+            #---------------------------
+
+            $EmailFrom = "[your send from email]"
+            $EmailTo = "[your send to email]"
+            $Subject = "$Computername Error $Service Not Running"
+            $Body = "Server: $Computername `n Service: $Service `n Status: Service not found or server not reachable `n $_"
+            $SMTPServer = "smtp.gmail.com"
+            $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+            $SMTPClient.EnableSsl = $true
+            $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("[your email prefix]", "[your email password]");
+            $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+ 
+        } 
+}
+
+                            
+ 
+ 
